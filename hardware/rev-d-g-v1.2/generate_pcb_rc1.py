dc409dca186a914bdfee310260f7f1690b6606a5
Keisuke Kusumoto <121006334+eieeeeeee@users.noreply.github.com>
2026-09-23 09:25:38 +0900

    Spread Rev.D-G RC1 placement and isolate geometry DRC

--- hardware/rev-d-g-v1.2/generate_pcb_rc1.py
+++ hardware/rev-d-g-v1.2/generate_pcb_rc1.py
@@ -46,28 +46,28 @@ sem=importlib.util.module_from_spec(spec); spec.loader.exec_module(sem)
 (CUSTOM/"TI_RAR0017B_LMG2100_DRAFT.kicad_mod").write_text(r'''(footprint "TI_RAR0017B_LMG2100_DRAFT"
   (version 20240108) (generator pcbnew)
   (layer "F.Cu")
-  (descr "DRAFT integration footprint; LMG2100 RAR0017B exact land/mask freeze required before fabrication")
+  (descr "DRAFT integration footprint; logical pads only; replace/verify against TI RAR0017B before fabrication")
   (attr smd)
-  (fp_rect (start -2.75 -2.25) (end 2.75 2.25) (stroke (width 0.2) (type default)) (fill none) (layer "F.SilkS"))
+  (fp_rect (start -2.75 -2.25) (end 2.75 2.25) (stroke (width 0.15) (type default)) (fill none) (layer "F.SilkS"))
   (fp_rect (start -3.05 -2.55) (end 3.05 2.55) (stroke (width 0.05) (type default)) (fill none) (layer "F.CrtYd"))
-  (fp_line (start -2.75 -2.25) (end -2.10 -2.25) (stroke (width 0.4) (type default)) (layer "F.SilkS"))
-  (pad "1" smd rect (at -2.35 -1.50) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "2" smd rect (at -2.35 -0.75) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "3" smd rect (at -2.35 0.00) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "4" smd rect (at -2.35 0.75) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "5" smd rect (at -1.35 1.85) (size 0.70 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "6" smd rect (at -0.45 1.85) (size 0.70 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "7" smd rect (at 0.45 1.85) (size 0.70 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "8" smd rect (at 1.35 1.85) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "9" smd rect (at 2.35 0.90) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "10" smd rect (at 2.35 0.30) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "11" smd rect (at 2.35 -0.30) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "12" smd rect (at 2.35 -0.90) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "13" smd rect (at 1.35 -1.85) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "14" smd rect (at 0.45 -1.85) (size 0.70 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "15" smd rect (at -0.45 -1.85) (size 0.70 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "16" smd rect (at -1.35 -1.85) (size 0.55 0.35) (layers "F.Cu" "F.Paste" "F.Mask"))
-  (pad "17" smd rect (at 0 0) (size 1.8 1.8) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (fp_line (start -2.75 -2.25) (end -2.05 -2.25) (stroke (width 0.35) (type default)) (layer "F.SilkS"))
+  (pad "1" smd rect (at -2.35 -0.75) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "2" smd rect (at -2.35 -0.25) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "3" smd rect (at -2.35 0.25) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "4" smd rect (at -2.35 0.75) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "5" smd rect (at -1.50 1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "6" smd rect (at -0.50 1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "7" smd rect (at 0.50 1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "8" smd rect (at 1.50 1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "9" smd rect (at 2.35 0.75) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "10" smd rect (at 2.35 0.25) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "11" smd rect (at 2.35 -0.25) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "12" smd rect (at 2.35 -0.75) (size 0.70 0.25) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "13" smd rect (at 1.50 -1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "14" smd rect (at 0.50 -1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "15" smd rect (at -0.50 -1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "16" smd rect (at -1.50 -1.90) (size 0.25 0.70) (layers "F.Cu" "F.Paste" "F.Mask"))
+  (pad "17" smd rect (at 0 0) (size 1.70 1.70) (layers "F.Cu" "F.Paste" "F.Mask"))
 )''',encoding="utf-8")
 
 def mm(x): return pcbnew.FromMM(float(x))
@@ -76,14 +76,14 @@ def v(x,y): return pcbnew.VECTOR2I(mm(x),mm(y))
 board=pcbnew.BOARD()
 board.SetCopperLayerCount(4)
 
-# Board outline 90 x 65 mm, origin at 0,0.
-for a,b in [((0,0),(90,0)),((90,0),(90,65)),((90,65),(0,65)),((0,65),(0,0))]:
+# Board outline 100 x 75 mm, origin at 0,0.
+for a,b in [((0,0),(100,0)),((100,0),(100,75)),((100,75),(0,75)),((0,75),(0,0))]:
     s=pcbnew.PCB_SHAPE(board)
     s.SetShape(pcbnew.SHAPE_T_SEGMENT); s.SetStart(v(*a)); s.SetEnd(v(*b)); s.SetLayer(pcbnew.Edge_Cuts)
     s.SetWidth(mm(0.1)); board.Add(s)
 
 # 4 x 3.2 mm NPTH mounting holes, kept outside the functional zones.
-for i,(x,y) in enumerate([(3.5,3.5),(86.5,3.5),(86.5,61.5),(3.5,61.5)],1):
+for i,(x,y) in enumerate([(3.5,3.5),(96.5,3.5),(96.5,71.5),(3.5,71.5)],1):
     fp=pcbnew.FOOTPRINT(board); fp.SetReference(f"H{i}"); fp.SetValue("M3_NPTH")
     fp.SetPosition(v(x,y))
     pad=pcbnew.PAD(fp); pad.SetNumber(""); pad.SetAttribute(pcbnew.PAD_ATTRIB_NPTH)
@@ -124,25 +124,25 @@ override={
 
 # Placement (mm), grouped by function.
 P={
-"J1":(5.5,45,90),"J2":(84.5,9,90),"J3":(45,4.5,0),
-"U1":(43,17,0),"U2":(17,18,0),"U3":(31,48,0),"U4":(43,48,0),"U5":(53,48,0),
-"U6":(75,31,0),"U7":(73,10,0),"U8":(61,10,0),"K1":(15,48,0),"Q1":(13,39,0),"Q2":(18,39,0),
-"FB101":(27,12,0),
-"R101":(38,8,0),"C101":(42,8,0),"R102":(47,8,0),
-"C110":(37,12,0),"C111":(40,12,0),"C112":(43,12,0),"C113":(46,12,0),"C114":(49,12,0),"C115":(30,12,0),"C116":(33,12,0),
-"R201":(9,22,0),"C201":(12,22,0),"R202":(15,22,0),"D201":(18,22,0),"D202":(21,22,0),"R203":(12,27,0),"C203":(15,27,0),
-"R210":(18,13,0),"R211":(21,13,0),"C210":(18,16,0),"C211":(21,16,0),"C212":(24,16,0),
-"C301":(25,52,90),"R301":(27,47,0),"R302":(27,51,0),"R303":(34,43,0),"C303":(37,43,0),"C304":(34,52,0),
-"R401":(40,42,0),"R402":(43,42,0),"R403":(46,42,0),"R404":(49,42,0),"R405":(53,42,0),"C405":(55,45,0),"C406":(57,45,0),
-"C501":(9,53,0),"R501":(12,53,0),"C502":(9,57,0),"R502":(12,57,0),"C503":(9,60,0),"R503":(12,60,0),
-"R601":(79,25,0),"R602":(79,28,0),"R603":(83,25,0),"R604":(83,28,0),
-"C601":(78,34,0),"C602":(81,34,0),"C603":(84,34,0),"C604":(78,38,0),"C605":(82,38,0),"C606":(86,38,0),
-"L601":(66,32,0),"C611":(64,37,0),"L602":(56,32,0),"C612":(54,37,0),"L603":(46,32,0),"C613":(44,37,0),
-"R701":(13,35,0),"R702":(16,35,0),"D701":(10,42,0),
-"L801":(69,10,0),"C801":(77,7,0),"C802":(80,7,0),"C803":(66,7,0),"C804":(63,7,0),"R805":(69,5,0),
-"C901":(58,7,0),"C902":(58,11,0),"C903":(58,14,0),
-"TP1":(6,39,0),"TP2":(10,30,0),"TP3":(34,56,0),"TP4":(56,52,0),"TP5":(72,37,0),
-"TP6":(42,32,0),"TP7":(66,14,0),"TP8":(53,12,0),"TP9":(24,18,0),"TP10":(8,35,0)
+"J1":(5.5,60,0),"J2":(94,12,0),"J3":(32,6,90),
+"U1":(48,19,0),"U2":(17,20,0),"U3":(32,62,0),"U4":(42,60,0),"U5":(50,60,0),
+"U6":(87,36,0),"U7":(82,11,0),"U8":(64,11,0),"K1":(59,61,0),"Q1":(70,60,0),"Q2":(76,60,0),
+"FB101":(31,29,0),
+"R101":(39,7,0),"C101":(43,7,0),"R102":(57,7,0),
+"C110":(39,29,0),"C111":(43,29,0),"C112":(47,29,0),"C113":(51,29,0),"C114":(55,29,0),"C115":(34,29,0),"C116":(36.5,29,0),
+"R201":(8,31,0),"C201":(12,31,0),"R202":(16,31,0),"D201":(20,31,0),"D202":(24,31,0),"R203":(11,36,0),"C203":(15,36,0),
+"R210":(10,10,0),"R211":(14,10,0),"C210":(10,15,0),"C211":(14,15,0),"C212":(22,15,0),
+"C301":(11,68,0),"R301":(18,68,0),"R302":(26,68,0),"R303":(31,55,0),"C303":(35,55,0),"C304":(35,68,0),
+"R401":(39,51,0),"R402":(43,51,0),"R403":(47,51,0),"R404":(51,51,0),"R405":(55,51,0),"C405":(54,55,0),"C406":(57,55,0),
+"C501":(12,45,0),"R501":(17,45,0),"C502":(12,50,0),"R502":(17,50,0),"C503":(12,55,0),"R503":(17,55,0),
+"R601":(91,29,0),"R602":(91,32,0),"R603":(96,29,0),"R604":(96,32,0),
+"C601":(87,41,0),"C602":(92,40,0),"C603":(96,40,0),"C604":(82,44,0),"C605":(87,45,0),"C606":(92,45,0),
+"L601":(74,37,0),"C611":(74,43,0),"L602":(61,37,0),"C612":(61,43,0),"L603":(48,37,0),"C613":(48,43,0),
+"R701":(68,52,0),"R702":(72,52,0),"D701":(62,52,0),
+"L801":(76,11,0),"C801":(88,18,0),"C802":(92,18,0),"C803":(74,17,0),"C804":(70,17,0),"R805":(80,6,0),
+"C901":(59,7,0),"C902":(59,15,0),"C903":(64,17,0),
+"TP1":(6,46,0),"TP2":(8,38,0),"TP3":(32,72,0),"TP4":(54,64,0),"TP5":(82,37,0),
+"TP6":(42,37,0),"TP7":(72,11,0),"TP8":(59,24,0),"TP9":(23,20,0),"TP10":(6,41,0)
 }
 
 footprints={}
@@ -172,25 +172,8 @@ def track(netname,p1,p2,width=0.25,layer=pcbnew.F_Cu):
 def direct(netname,a,b,width=0.25,layer=pcbnew.F_Cu):
     track(netname,pad(*a).GetPosition(),pad(*b).GetPosition(),width,layer)
 
-# RC1 critical-loop routing only. The remaining nets stay intentionally unrouted
-# for the first DRC pass; this lets us quantify the exact remaining work.
-critical=[
- ("GAN_HB",("U6","10"),("C601","1"),0.40),
- ("GAN_SW",("U6","11"),("C601","2"),0.80),
- ("GAN_SW",("U6","5"),("L601","1"),1.20),
- ("GAN_HI",("U6","12"),("R601","2"),0.30),
- ("GAN_LI",("U6","13"),("R602","2"),0.30),
- ("GAN_F1",("L601","2"),("L602","1"),0.90),
- ("GAN_F2",("L602","2"),("L603","1"),0.90),
- ("ACTIVE_OUT",("L603","2"),("C301","1"),0.70),
- ("INJ_C_NODE",("C301","2"),("R301","1"),0.60),
- ("INJ_R_NODE",("R301","2"),("R302","2"),0.60),
- ("INJ_LINE",("R302","1"),("K1","5"),0.60),
- ("+12V",("J2","1"),("C604","1"),1.00),
- ("+12V",("C604","1"),("U6","7"),1.00),
- ("+5V",("L801","2"),("C803","1"),0.60),
- ("+3V3",("U8","5"),("C902","1"),0.40),
-]
+# RC1b placement-clean pass: routing intentionally disabled until geometry is clean.
+critical=[]
 for n,a,b,w in critical:
     try: direct(n,a,b,w)
     except Exception as e: print("CRITICAL_ROUTE_SKIPPED",n,a,b,e)
@@ -213,4 +196,4 @@ print("BOARD",BOARD_FILE)
 print("FOOTPRINTS",len(footprints))
 print("NETS",len(nets))
 print("COPPER_LAYERS",board.GetCopperLayerCount())
-print("SIZE_MM","90 x 65")
+print("SIZE_MM","100 x 75")
